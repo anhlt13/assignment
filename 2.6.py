54fc79b63c7d02a66ac6d388a756f3a68d0a39df
tranlananh <anhhllan246@gmail.com>
2024-08-23 11:14:12 +0300

    exercise week 1

---
+++ 2.6.py
@@ -0,0 +1,14 @@
+"""
+import random
+number_1= random.randint(0,9)
+number_2= random.randint(0,9)
+number_3= random.randint(0,9)
+print(f"The code is: {number_1}{number_2}{number_3} ")
+"""
+
+import random
+number_1 = random.randint(1,6)
+number_2 = random.randint(1,6)
+number_3 = random.randint(1,6)
+number_4 = random.randint(1,6)
+print(f"the code is: {number_1}{number_2}{number_3}{number_4}")
